9e84718e1cb464fc810fe846a868bee1ae34a497
dblyons <dlyons@berkeley.edu>
2018-10-04 14:57:14 -0700

    Add files via upload

---
+++ calculate_NRL.R
@@ -0,0 +1,27 @@
+#calculate NRL estimate
+
+filenames <- list.files("/path/to/your/numap/output/",
+                        pattern=".peak_pos.txt$", full.names=TRUE)
+
+your_input <- lapply(filenames, read.table)
+
+resultlis<-list()
+nrl.result<-list()
+stderr.result<-list()
+
+
+for (i in 1:length(filenames)) {  
+
+  resultlis[[i]]<-summary(lm(c(0, your_input[[i]]$V2[c(1:4)]) ~ c(1:5)))
+  nrl.result[[i]]<- c(resultlis[[i]]$coefficient[2])
+  stderr.result[[i]]<-c(resultlis[[i]]$coefficient[4])
+  
+}
+
+together.nrl<-do.call(cbind, nrl.result)
+together.err<-do.call(cbind, stderr.result)
+#use together for generating barplot with error bars
+together<-data.frame(id=basename(filenames), nrl=as.vector(together.nrl), err=as.vector(together.err))
+
+
+
