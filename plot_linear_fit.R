0c0f6f9143a853be5e9402e39e977c31271ef4c2
dblyons <dlyons@berkeley.edu>
2018-10-04 15:45:55 -0700

    Add files via upload

---
+++ plot_linear_fit.R
@@ -0,0 +1,44 @@
+#plot linear fit of dyad-to-dyad peaks for visualization of regression calculation
+
+filenames <- list.files("/path/to/your/numap/output/",
+                        pattern=".peak_pos.txt$", full.names=TRUE)
+
+your_input <- lapply(filenames, read.table)
+
+resultlis<-list()
+
+for (i in 1:length(filenames)) {  
+  
+  # populate the list with previously matched of loci
+  # 
+  #use col 2 for peak_pos.txt files if want to plot fit
+  resultlis[[i]]<-c(0, your_input[[i]][c(1:3),2])
+  #resultlis[[i]]<-h1[[i]][,3]
+  
+}
+
+together<-do.call(cbind, c(resultlis))
+together<-as.data.frame(together)
+names(together)<-basename(filenames)
+#if plotting phasogram use this one
+#together<-data.frame(pos=seq(1,3001), together)
+#for fit of phaso use this
+together<-data.frame(pos=c(1:(length(resultlis[[1]]))), together)
+melt.together<-melt(together, id='pos')
+
+#user-defined variables determined by basename - in the end, you want genotype and replicate to be combined for color ID
+melt_thing<-str_split_fixed(melt.together$variable, '\\.', 10)
+meltbig<-data.frame(melt.together, genotype=melt_thing[,1], replicate=melt_thing[,2], exp.level=melt_thing[,5])
+
+meltbig<-data.frame(meltbig, genotypeByreplicate=paste(meltbig$genotype, 
+                                                       meltbig$replicate))
+
+ggplot(meltbig, aes(pos, value, color=genotypeByreplicate))+
+  geom_point( size=3, alpha=I(2/3) )+stat_smooth(method='lm', se=FALSE, alpha=I(1/2))+
+  theme_bw(base_size = 15)+
+  facet_grid(.~exp.level, scales="free_y")+
+  scale_color_manual(values =
+                       c(  "blue3", "steelblue",
+                           "forestgreen", "mediumseagreen"
+                       ))
+
---
+++ plot_phasogram.R
@@ -0,0 +1,40 @@
+#plot linear fit of dyad-to-dyad peaks for visualization of regression calculation
+
+filenames <- list.files("/path/to/your/numap/output/",
+                        pattern="phasogram", full.names=TRUE)
+
+your_input <- lapply(filenames, read.table)
+
+resultlis<-list()
+
+for (i in 1:length(filenames)) {  
+
+  resultlis[[i]]<-h1[[i]][,3]
+  
+}
+
+together<-do.call(cbind, c(resultlis))
+together<-as.data.frame(together)
+names(together)<-basename(filenames)
+together<-data.frame(pos=seq(1,3001), together)
+
+together<-data.frame(pos=c(1:(length(resultlis[[1]]))), together)
+melt.together<-melt(together, id='pos')
+
+#user-defined variables determined by basename - in the end, you want genotype and replicate to be combined for color ID
+melt_thing<-str_split_fixed(melt.together$variable, '\\.', 10)
+meltbig<-data.frame(melt.together, genotype=melt_thing[,1], replicate=melt_thing[,2], exp.level=melt_thing[,5])
+
+meltbig<-data.frame(meltbig, genotypeByreplicate=paste(meltbig$genotype, 
+                                                       meltbig$replicate))
+
+ggplot(meltbig, aes(pos, value, color=genotypeByreplicate))+
+  geom_line( )+
+  theme_bw(base_size = 15)+
+  facet_wrap(~exp.level, 1,4, scales="free_y")+
+  xlim(0,750)+
+  scale_color_manual(values =
+                       c(  "blue3", "steelblue",
+                           "forestgreen", "mediumseagreen"
+                       ))
+
